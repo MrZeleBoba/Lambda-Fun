65f1024fc9252b1699562a2b000824f151ba951d
Aleksey Galishchev <galishchev@inbox.ru>
2023-02-10 07:25:59 +0300

    Program

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args){
+        List<Person> person = new ArrayList<>();
+        person.add(new Person("Petya", "Sorokin", 10));
+        person.add(new Person("Olya", "Kosolapova", 15));
+        person.add(new Person("Dima", "Ivanov", 13));
+        person.add(new Person("Николай","Римский-Корсаков",50));
+        person.add(new Person("Николай","Миклухо-Маклай",60));
+
+        Collections.sort(person, new PersonComparator());
+        System.out.println(person);
     }
 }
---
+++ src/PersonComparator.java
@@ -0,0 +1,21 @@
+import java.util.Comparator;
+
+public class PersonComparator implements Comparator<Person> {
+    private int maxLength;
+
+    public PersonComparator() {
+        this.maxLength = maxLength;
+    }
+
+    @Override
+    public int compare(Person o1, Person o2) {
+        int length1 = o1.getSurname().split("(?U)\\W").length;
+        int length2 = o2.getSurname().split("(?U)\\W").length;
+        if (Math.min(length1, maxLength) != Math.min(length2, maxLength)) {
+            return Integer.compare(length1, length2);
+        }
+        return Integer.compare(o1.getAge(), o2.getAge());
+    }
+
+}
+
